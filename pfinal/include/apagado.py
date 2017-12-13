6e09f20ce50570f0c09e88c2230ea1dd7aada74f
rodmendoza07 <lr.mendozar@gmail.com>
2017-12-13 21:59:22 +0000

    enciende y apaga

---
+++ pfinal/include/apagado.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM) #GPIO.BCM o GPIO.BOARD
+GPIO.setup(4, GPIO.OUT) #El pin GPIO4 se configura como salida
+                        #Si se quiere configurar como entrada se coloca GPIO.setup(4, GPIO.IN)
+def apagado():        #Se declara una funcion con cero argumentos
+    GPIO.output(4, False) #El pin se enciende cuando el segundo argumento es True, si se desea apagar se pone False
+    print("pin 4 apagado")
+
+apagado()
