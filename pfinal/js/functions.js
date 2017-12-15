00fc934fcc15a2a39a94d45cb6a9ab5c5cac4885
Rodmendoza <lr.mendozar@gmail.com>
2017-12-14 23:09:29 -0600

    async false

--- pfinal/js/functions.js
+++ pfinal/js/functions.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
                 url: 'include/callpython.php',
                 data: {option: 0},
                 contentType: 'application/json; charset=utf-8',
+                async: false,
                 beforeSend: function(){
                     console.log("Apagando dispositivo");
                 },
